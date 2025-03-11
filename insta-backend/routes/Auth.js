cab10b0283d88dcb5c6573cf2c66d90f439349e7
madhavi-neupane <neupanem206@gmail.com>
2025-03-12 00:18:12 +0545

    backend

---
+++ insta-backend/routes/Auth.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/Users')
+
+router.post('/signup', async (req, res) => {
+  try {
+    const { username, email, password } = req.body
+
+    const existingUser = await User.findOne({ email })
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' })
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const newUser = new User({ username, email, password: hashedPassword })
+    await newUser.save()
+
+    res.status(201).json({ message: 'User registered successfully !' })
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+})
+
+module.exports = router
---
+++ insta-backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+dotenv.config()
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.error('MongoDB connection error:', err))
+
+app.get('/', (req, res) => {
+  res.send('API is running...')
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+const authRoutes = require('./routes/Auth')
+app.use('/api/Auth', authRoutes)
